e76de3ecb8372212cb963a210f61fe3be1f698c9
amorriesette <afmorriesette@gmail.com>
2017-07-29 08:50:22 -0500

    Corrected chart to display current stats.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var _HoodExdata = {
+
+	createSkinsTable: function(){
+		//method to append table for skins
+	}
+
+	createScoringTable: function(){
+		//method to append table for scoring
+	}
+}
+
+
+
